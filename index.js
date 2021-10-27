fdcc67a9ac922a5a2eb892fba6d8e690dea6970f
Lana Souza <laniziamunique@gmail.com>
2021-10-27 13:45:27 -0300

    Updates

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import cipher from './cipher.js';
+import cipher from "./cipher.js";
 
 const offsetInput = document.querySelector("#keyInput");
 const wordInput = document.querySelector("#textInput");
@@ -6,26 +6,19 @@ const result = document.querySelector("#result");
 const btnEncrypt = document.querySelector("#encrypt");
 const btnDecrypt = document.querySelector("#decrypt");
 
-function onEncrypt () { 
-    const offset = parseInt(offsetInput.value);
-    const word = wordInput.value;
-    wordInput.value = "";
-    result.value = cipher.encode(offset, word);
+function onEncrypt() {
+  const offset = parseInt(offsetInput.value);
+  const word = wordInput.value;
+  wordInput.value = "";
+  result.value = cipher.encode(offset, word);
+}
+
+function onDecrypt() {
+  const offset = parseInt(offsetInput.value);
+  const word = wordInput.value;
+  wordInput.value = "";
+  result.value = cipher.decode(offset, word);
 }
 
 btnEncrypt.addEventListener("click", onEncrypt);
-
-function onDecrypt () {
-    const offset = parseInt(offsetInput.value);
-    const word = wordInput.value;
-    wordInput.value = "";
-    result.value = cipher.decode(offset, word);
-}
-
 btnDecrypt.addEventListener("click", onDecrypt);
-
-
-
-
-
-
